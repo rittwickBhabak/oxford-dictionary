5d5cb43db392755a3f2fcd09aabc2b0af5236bfb
Rittwick Bhabak <rittwick2019@gmail.com>
2022-03-20 11:59:10 +0530

    always input added

--- dictionary.py
+++ dictionary.py
@@ -14,10 +14,11 @@ if len(sys.argv)>1:
         input('Some error occoured. Press ENTER to EXIT.')
 else:
     try:
-        word = input('Enter the word: ')
-        senses = get_senses(word)
-        print_meaning(word, senses)
-        input('Press ENTER to exit.')
+        while True:
+            word = input('Enter the word: ')
+            senses = get_senses(word)
+            print_meaning(word, senses)
+        
     except Exception as e:
         print(f"ERROR: {str(e)}")
         input('Some error occoured. Press ENTER to EXIT.')
