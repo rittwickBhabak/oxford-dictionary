ab62f821e0303b779358643a8c1557ee6c20753f
Rittwick Bhabak <rittwick2019@gmail.com>
2022-03-21 10:52:36 +0530

    write_output added

--- oxford_dictionary/get_response.py
+++ oxford_dictionary/get_response.py
@@ -6,9 +6,18 @@ APP_ID = CREDENTIALS.get('app_id')
 APP_KEY = CREDENTIALS.get('api_key')
 language = "en-gb"
 
+def write_output(word, response, debug):
+    if not debug:
+        return 
+        
+    with open(f"{word}.json", 'w', encoding='utf8') as f:
+        f.write(str(response.text))
+
 def meaning(word_id):
     url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
     r = requests.get(url, headers={"app_id": APP_ID, "app_key": APP_KEY})
+    debug = True
+    write_output(word_id, r, debug)
     return r 
 
 if __name__=="__main__":
